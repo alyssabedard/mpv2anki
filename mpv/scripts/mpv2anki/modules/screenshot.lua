bd6046ad99187ab58e5f7670f02e28b5d1d2a66b
Alyssa Bédard <70352767+alyssabedard@users.noreply.github.com>
2025-10-11 20:01:08 -0400

    FEATURE #4 Allow user to resize screenshot image

--- mpv/scripts/mpv2anki/modules/screenshot.lua
+++ mpv/scripts/mpv2anki/modules/screenshot.lua
@@ -2,6 +2,7 @@
 -- scripts/mpv2anki/modules/screenshot.lua
 -----------------------------------------------------------------------
 -- Take screenshot using mpv's native screenshot command
+-- TODO: More error handling
 -----------------------------------------------------------------------
 local mp = require('mp')
 local msg = require('mp.msg')
@@ -9,18 +10,56 @@ local config = require('config')
 
 local screenshot = {}
 
-function screenshot.create_screenshot(filename)
 
-    local success = mp.commandv("screenshot-to-file", 
-        config.ANKI.MEDIA_PATH .. filename,
-        "video"  -- Only capture video, no subtitles (text)
+-- If set to true in script-opts/mpv2anki.conf,
+-- this function will resize the screenshot image
+function screenshot.resize_image(input_path, output_path, width, height)
+
+    quality = 10
+
+    local result = mp.command_native({
+        name = "subprocess",
+        args = {
+            "ffmpeg",
+            "-i", input_path,
+            "-vf", string.format("scale=%d:%d", width, height), -- Video Filter flag
+            "-q:v", tostring(quality),
+            "-y",
+            output_path
+        },
+        playback_only = false,
+        capture_stdout = true,
+        capture_stderr = true
+    })
+
+    return result.status == 0
+end
+
+
+function screenshot.create_screenshot(filename)
+    local full_path = config.ANKI.MEDIA_PATH .. filename
+
+    -- https://mpv.io/manual/master/#command-interface-screenshot-to-file
+    local res = mp.commandv("screenshot-to-file",
+            full_path,
+            "video"  -- Only capture video, no subtitles (text)
     )
-    if not success then
+
+    if not res then
         msg.error("Failed to create screenshot")
         return
     end
-    
-    return success
+
+    if config.MEDIA.RESIZE_IMAGE == "true" then
+        res = screenshot.resize_image(full_path, full_path,640, 360)
+        -- res = screenshot.resize_image(full_path, full_path, 320, 180)
+        if not res then
+            msg.error("Failed to resize screenshot")
+            return
+        end
+    end
+
+    return res
 end
 
 return screenshot
